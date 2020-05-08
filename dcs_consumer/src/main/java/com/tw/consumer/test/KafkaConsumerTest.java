14c6e0df2f71595913d9aad08416d1150281397d
Administrator <Administrator@10.1.175.64>
2020-05-08 18:36:07 +0800

    init

---
+++ dcs_consumer/src/main/java/com/tw/consumer/test/KafkaConsumerTest.java
@@ -0,0 +1,27 @@
+package com.tw.consumer.test;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+public class KafkaConsumerTest {
+	public static void main(String[] args) {
+		Properties props = new Properties();
+	     props.put("bootstrap.servers", "10.0.0.187:9092,10.0.0.188:9092,10.0.0.189:9092");
+	     props.put("group.id", "eeeee");
+	     props.put("enable.auto.commit", "true");
+	     props.put("auto.commit.interval.ms", "1000");
+	     props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+	     props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+	     KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+	     consumer.subscribe(Arrays.asList("mqtttest"));
+	     while (true) {
+	         ConsumerRecords<String, String> records = consumer.poll(100);
+	         for (ConsumerRecord<String, String> record : records)
+	             System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+	     }
+	}
+}
